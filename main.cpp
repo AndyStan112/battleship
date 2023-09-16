f93bd68651431ad279fdc86e3eec02f597ec17a0
AndyStan112 <stanica.andrei28@gmail.com>
2023-09-16 23:00:04 +0300

    add battleship rotation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <Array>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 class GameTile {
     public:
@@ -13,8 +14,40 @@ class GameTile {
         offset=user=="enemy"?640:0;
         shape->setFillColor(sf::Color::Cyan);
         shape->setOutlineThickness(2);
-        shape->setPosition(10 + 41 * i + offset, 10+ 41*j);
+        shape->setPosition(10 + 40 * i + offset, 10+ 40*j);
     }
+};
+
+class BattleShip {
+    public:
+    int length= 2;
+    int width=40*length-8;
+    int height=32;
+    sf::RectangleShape* shape ;
+    int offset;
+    std::string direction= "vertical";
+    BattleShip(){
+        
+        shape = new sf::RectangleShape(sf::Vector2f(width, height));
+        // offset=user=="enemy"?640:0;
+        shape->setFillColor(sf::Color::Red);
+        shape->setOutlineThickness(1);
+        shape->setPosition(13, 13);
+    }
+    BattleShip& rotate(){
+        if(direction=="vertical"){
+            direction="horizontal";
+            std::swap(width,height);
+            shape->setSize(sf::Vector2f(width,height));
+        }
+        else{
+            direction="vertical";
+            std::swap(width,height);
+            shape->setSize(sf::Vector2f(width,height));
+        }
+          return *this;
+    }
+  
 };
 
 class GameGrid {
@@ -40,17 +73,24 @@ int main()
     sf::RenderWindow window(sf::VideoMode(1080, 720), "SFML works!");
     GameGrid userGrid= GameGrid("user");
     GameGrid enemyGrid= GameGrid("enemy");
-    
+    BattleShip battleship = BattleShip();
     while (window.isOpen())
     {
         sf::Event event;
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
         while (window.pollEvent(event))
-        {
+        {   
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::R) {
+                    battleship.rotate();
+                }
+             }
         }
 
         window.clear();
+        //draw
         for(auto row: userGrid.grid){
            for(auto tile: row){
             window.draw(*tile->shape);
@@ -61,7 +101,7 @@ int main()
             window.draw(*tile->shape);
            }
         }
-       
+       window.draw(*battleship.shape);
         window.display();
     }
 
